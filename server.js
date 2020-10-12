16a1cbfcc7b19d0a376dc35db399946792ca9a5f
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-12 12:13:22 -0500

    reorder routes

--- server.js
+++ server.js
@@ -80,6 +80,74 @@ app.get('/api/candidate/:id', (req, res) => {
   });
 });
 
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// // ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
+
+// Create a candidate
+app.post('/api/candidate', ({
+  body
+}, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({
+      error: errors
+    });
+    return;
+  }
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
+              VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+  // ES5 function, not arrow function, to use `this`
+  db.run(sql, params, function (err, result) {
+    if (err) {
+      res.status(400).json({
+        error: err.message
+      });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: body,
+      id: this.lastID
+    });
+  });
+});
+
+app.put('/api/candidate/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'party_id');
+
+if (errors) {
+  res.status(400).json({ error: errors });
+  return;
+}
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
+
 // Delete a candidate
 // db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
 //     if (err) {
@@ -102,43 +170,6 @@ app.delete('/api/candidate/:id', (req, res) => {
 
     res.json({
       message: 'successfully deleted',
-      changes: this.changes
-    });
-  });
-});
-
-// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-// // ES5 function, not arrow function, to use this
-// db.run(sql, params, function(err, result) {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result, this.lastID);
-// });
-
-app.put('/api/candidate/:id', (req, res) => {
-  const errors = inputCheck(req.body, 'party_id');
-
-if (errors) {
-  res.status(400).json({ error: errors });
-  return;
-}
-  const sql = `UPDATE candidates SET party_id = ? 
-               WHERE id = ?`;
-  const params = [req.body.party_id, req.params.id];
-
-  db.run(sql, params, function(err, result) {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: req.body,
       changes: this.changes
     });
   });
@@ -186,37 +217,6 @@ app.delete('/api/party/:id', (req, res) => {
     }
 
     res.json({ message: 'successfully deleted', changes: this.changes });
-  });
-});
-
-// Create a candidate
-app.post('/api/candidate', ({
-  body
-}, res) => {
-  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
-  if (errors) {
-    res.status(400).json({
-      error: errors
-    });
-    return;
-  }
-  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
-              VALUES (?,?,?)`;
-  const params = [body.first_name, body.last_name, body.industry_connected];
-  // ES5 function, not arrow function, to use `this`
-  db.run(sql, params, function (err, result) {
-    if (err) {
-      res.status(400).json({
-        error: err.message
-      });
-      return;
-    }
-
-    res.json({
-      message: 'success',
-      data: body,
-      id: this.lastID
-    });
   });
 });
 
